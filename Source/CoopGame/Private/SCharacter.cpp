2058563f974a91af298c64712e3873225a82286e
daluowudi <409627982@qq.com>
2019-05-04 21:45:18 +0800

    提交添加摄像机和mesh

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -8,6 +10,12 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("ArmComp"));
+	ArmComp->SetupAttachment(RootComponent);
+	ArmComp->bUsePawnControlRotation = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(ArmComp);
 }
 
 // Called when the game starts or when spawned
