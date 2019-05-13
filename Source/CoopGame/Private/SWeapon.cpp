0891a860decc3b77ccf6bc2e2e5a88cc216437cc
lichaoye <lichaoye@playcrab.com>
2019-05-13 09:04:54 +0800

    ConsoleDebug

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -6,6 +6,13 @@
 #include "Kismet/GameplayStatics.h"
 #include "Particles/ParticleSystemComponent.h"
 
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef CVARDebugWeaponDrawing(
+	TEXT("COOP.DebugWeapons"),
+	DebugWeaponDrawing,
+	TEXT("Draw Debug Lines for Weapons"),
+	ECVF_Cheat);
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -61,7 +68,10 @@ void ASWeapon::Fire()
 			}
 		}
 
-		DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor::White, false, 1, 0, 2);
+		if (DebugWeaponDrawing > 0)
+		{
+			DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor::White, false, 1, 0, 2);
+		}		
 
 		if (MuzzleEffect)
 		{
