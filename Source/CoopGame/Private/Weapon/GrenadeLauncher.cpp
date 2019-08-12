6e66dde14094495b6c9acb856b1d95fb59575a26
lichaoye <lichaoye@playcrab.com>
2019-08-12 18:59:37 +0800

    修改子弹出生位置

--- Source/CoopGame/Private/Weapon/GrenadeLauncher.cpp
+++ Source/CoopGame/Private/Weapon/GrenadeLauncher.cpp
@@ -21,7 +21,8 @@ void AGrenadeLauncher::Fire()
 		SpawnParams.Owner = MyOwner; // 这里暂时把owner设置为character
 		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
-		ASLauncherProjectile* Projectile = GetWorld()->SpawnActor<ASLauncherProjectile>(ProjectileClass, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
+		FTransform MuzzleTransform = MeshComp->GetSocketTransform(MuzzleSocketName, RTS_World);
+		ASLauncherProjectile* Projectile = GetWorld()->SpawnActor<ASLauncherProjectile>(ProjectileClass, MuzzleTransform.GetLocation(), FRotator::ZeroRotator, SpawnParams);
 
 		// 将子弹发射出去
 		FVector StartPoint;
